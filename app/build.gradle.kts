5af53c06a73f2b292dad583518fa0423d0bf1659
Pasupathiv47 <pasupathiv15@gmail.com>
2026-09-24 08:02:13 +0530

    Add initial build configuration for Android app

---
+++ app/build.gradle.kts
@@ -0,0 +1,25 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "com.pasupathi.bustracker"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.pasupathi.bustracker"
+        minSdk = 24
+        targetSdk = 34
+        versionCode = 1
+        versionName = "0.1"
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+}
