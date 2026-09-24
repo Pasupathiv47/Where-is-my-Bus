95d5a89a85d455cbd96e0edacf6d316e5f51fe20
Pasupathiv47 <pasupathiv15@gmail.com>
2026-09-24 11:16:20 +0530

    Update plugin versions in build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
-    id("com.android.application")
-    id("org.jetbrains.kotlin.android")
-    id("com.google.gms.google-services")
+    id("com.android.application") version "8.5.2" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.24" apply false
+    id("com.google.gms.google-services") version "4.4.2" apply false
 }
 
 android {
